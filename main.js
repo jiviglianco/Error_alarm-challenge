d281167cd2b7ede207932b192cc6e66a2cf92c56
Juan Ignacio Viglianco <jiviglianco@gmail.com>
2024-01-21 21:56:50 -0300

    Initial Commit

---
+++ main.js
@@ -0,0 +1,49 @@
+// Global variables
+var errorCount = 0
+var lastNotificationTime = null
+
+// Function to log errors
+function logError (error) {
+    appendToLogFile(error)
+    errorCount++
+    checkErrorThreshold()
+}
+
+// Function to check if the error threshold is reached
+function checkErrorThreshold () {
+    var currentTime = getCurrentTime()
+
+    // Check if a minute has passed since the last notification
+    if (lastNotificationTime === null || currentTime - lastNotificationTime >= 60) {
+        // Reset error count and update last notification time
+        // Resetting the error count to 1 because the current error has been added already
+        errorCount = 1
+        lastNotificationTime = currentTime
+    }
+
+    // Check if error count exceeds the threshold
+    if (errorCount > 10) {
+        // Trigger email notification
+        sendEmailNotification("High error rate detected", "More than 10 errors occurred in the last minute.")
+
+        // Reset error count and update last notification time
+        errorCount = 0
+        lastNotificationTime = currentTime
+    }
+}
+
+// Function to get the current time
+function getCurrentTime () {
+    // Return the current time in seconds
+}
+
+// Function to send email notification
+function sendEmailNotification (subject, message) {
+    // Include the specified subject and message in the email
+    // Maybe we can add a link to the log file in the email or attach the log file
+}
+
+// Function to append errors to the log file
+function appendToLogFile (error) {
+    // Include the specified error in the end of the log file
+}
